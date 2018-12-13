b7bdd68c863fee5c61b741dd6f4e5bffb81455e3
Alex Sligar <info@gravelroad.co>
2018-12-13 13:47:57 -0800

    Add initialData reducer and tests

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,5 @@
 import { combineReducers } from 'redux';
-import initialData from './shared';
+import initialData from './initialData';
 
 export default combineReducers({
     initialData,
---
+++ src/reducers/initialData.js
@@ -0,0 +1,37 @@
+import {
+    INITIAL_DATA_LOADING,
+    INITIAL_DATA_SUCCESS,
+    INITIAL_DATA_ERROR,
+} from '../actions/initialData';
+
+const initialState = {
+    loading: true,
+    error: null,
+};
+
+
+export default function initialData (state = initialState, action) {
+
+    switch (action.type) {
+        case INITIAL_DATA_LOADING :
+            return {
+                ...state,
+                loading: true,
+                error: null,
+            };
+        case INITIAL_DATA_SUCCESS :
+            return {
+                ...state,
+                loading: false,
+                error: null,
+            }
+        case INITIAL_DATA_ERROR :
+            return {
+                ...state,
+                loading: false,
+                error: action.error,
+            }
+        default :
+            return state;
+    }
+}
