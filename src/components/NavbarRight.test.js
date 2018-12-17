3185b57875ed5b76210bdaebd3e7afa76de01f88
Alex Sligar <info@gravelroad.co>
2018-12-17 12:59:47 -0800

    Move tests out of describe, not needed

--- src/components/NavbarRight.test.js
+++ src/components/NavbarRight.test.js
@@ -10,54 +10,49 @@ const setup = (props = {}) => {
 
 describe('render', () => {
 
-    describe('when not logged in', () => {
+    it('should render the correct links when not at /login or /register', () => {
 
-        it('should render the correct links when not at /login or /register', () => {
-
-            const props = {
-                location: {
-                    pathname: '/',
-                },
-            }
-            const wrapper = setup(props);
-            const menu = wrapper.find(Menu.Menu);
-            expect(menu.length).toBe(1);
-            const registerLink = wrapper.find('Link[to="/register"]');
-            expect(registerLink.length).toBe(1);
-            const loginLink = wrapper.find('Link[to="/login"]');
-            expect(loginLink.length).toBe(1);
-        });
-
-        it('should not render a register link when at /register', () => {
-
-            const props = {
-                location: {
-                    pathname: '/register',
-                },
-            }
-            const wrapper = setup(props);
-            const menu = wrapper.find(Menu.Menu);
-            expect(menu.length).toBe(1);
-            const registerLink = wrapper.find('Link[to="/register"]');
-            expect(registerLink.length).toBe(0);
-            
-        });
-
-        it('should not render a register link when at /login', () => {
-
-            const props = {
-                location: {
-                    pathname: '/login',
-                },
-            }
-            const wrapper = setup(props);
-            const menu = wrapper.find(Menu.Menu);
-            expect(menu.length).toBe(1);
-            const registerLink = wrapper.find('Link[to="/login"]');
-            expect(registerLink.length).toBe(0);
-            
-        });
+        const props = {
+            location: {
+                pathname: '/',
+            },
+        }
+        const wrapper = setup(props);
+        const menu = wrapper.find(Menu.Menu);
+        expect(menu.length).toBe(1);
+        const registerLink = wrapper.find('Link[to="/register"]');
+        expect(registerLink.length).toBe(1);
+        const loginLink = wrapper.find('Link[to="/login"]');
+        expect(loginLink.length).toBe(1);
     });
 
-    
+    it('should not render a register link when at /register', () => {
+
+        const props = {
+            location: {
+                pathname: '/register',
+            },
+        }
+        const wrapper = setup(props);
+        const menu = wrapper.find(Menu.Menu);
+        expect(menu.length).toBe(1);
+        const registerLink = wrapper.find('Link[to="/register"]');
+        expect(registerLink.length).toBe(0);
+        
+    });
+
+    it('should not render a register link when at /login', () => {
+
+        const props = {
+            location: {
+                pathname: '/login',
+            },
+        }
+        const wrapper = setup(props);
+        const menu = wrapper.find(Menu.Menu);
+        expect(menu.length).toBe(1);
+        const registerLink = wrapper.find('Link[to="/login"]');
+        expect(registerLink.length).toBe(0);
+        
+    });
 });
