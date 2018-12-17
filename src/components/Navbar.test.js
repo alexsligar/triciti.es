ea2315cad6a70b459ed91834f0ec561f2ca0b8eb
Alex Sligar <info@gravelroad.co>
2018-12-17 13:01:00 -0800

    Tests to check if correct child components are rendered in Navbar

--- src/components/Navbar.test.js
+++ src/components/Navbar.test.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import { Menu } from 'semantic-ui-react';
-import Navbar from './Navbar';
+import { Navbar } from './Navbar';
+import NavbarRight from './NavbarRight';
+import NavbarRightAuthed from './NavbarRightAuthed';
 
 const setup = (props = {}) => {
     const wrapper = shallow(<Navbar {...props} />);
@@ -10,10 +12,27 @@ const setup = (props = {}) => {
 
 describe('render', () => {
 
-    it('should render component without error', () => {
+    it('should render correct child components when no authedUser', () => {
 
-        const wrapper = setup();
+        const props = { authedUser: null };
+        const wrapper = setup(props);
         const menu = wrapper.find(Menu);
         expect(menu.length).toBe(1);
+        const navbarRight = wrapper.find(NavbarRight);
+        expect(navbarRight.length).toBe(1);
+        const navbarRightAuthed = wrapper.find(NavbarRightAuthed);
+        expect(navbarRightAuthed.length).toBe(0);
+    });
+
+    it('should render correct child components when authedUser', () => {
+
+        const props = { authedUser: 'test' };
+        const wrapper = setup(props);
+        const menu = wrapper.find(Menu);
+        expect(menu.length).toBe(1);
+        const navbarRight = wrapper.find(NavbarRight);
+        expect(navbarRight.length).toBe(0);
+        const navbarRightAuthed = wrapper.find(NavbarRightAuthed);
+        expect(navbarRightAuthed.length).toBe(1);
     });
 });
