94ea89ed351284148c2c40bd750628d3e8d23056
Alex Sligar <info@gravelroad.co>
2018-12-17 02:05:42 -0800

    Clean up file and remove unneeded import

--- src/components/users/Register.js
+++ src/components/users/Register.js
@@ -1,18 +1,18 @@
 import React from 'react';
-import { Container, Grid, Header } from 'semantic-ui-react';
+import { Grid, Header } from 'semantic-ui-react';
 import RegisterForm from './RegisterForm';
 
 export default function Register() {
     return (
-                <Grid textAlign='center' style={{ height: '100%' }} verticalAlign='middle' className='greyBackground'>
-                    <Grid.Row>
-                        <Grid.Column width={6}>
-                            <Header as='h2' textAlign='center'>
-                                Register for an Account
-                            </Header>
-                            <RegisterForm />
-                        </Grid.Column>
-                    </Grid.Row>
-                </Grid>
+        <Grid textAlign='center' style={{ height: '100%' }} verticalAlign='middle' className='greyBackground'>
+            <Grid.Row>
+                <Grid.Column width={6}>
+                    <Header as='h2' textAlign='center'>
+                        Register for an Account
+                    </Header>
+                    <RegisterForm />
+                </Grid.Column>
+            </Grid.Row>
+        </Grid>
     );
 } 
