45d67435ce43559e73e93fd15cea1548a590af12
Alex Sligar <info@gravelroad.co>
2018-12-20 23:22:24 -0800

    Update reference to renamed file

--- src/components/tags/TagSearch.js
+++ src/components/tags/TagSearch.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import { Search, Icon } from 'semantic-ui-react';
-import { handleGetTags } from '../../actions/tags';
+import { handleGetTags } from '../../actions/tags/getTags';
 
 export class TagSearch extends Component {
     state = {
