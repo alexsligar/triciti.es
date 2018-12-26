4f754844611861c1e854464b592b5333e6bbfb33
Alex Sligar <info@gravelroad.co>
2018-12-26 15:04:44 -0800

    Add handleAddItem and removeAddItemError action creators with tests

---
+++ src/actions/items/addItem.js
@@ -0,0 +1,56 @@
+export const ADD_ITEM_PROCESSING = 'ADD_ITEM_PROCESSING';
+export const ADD_ITEM_ERROR = 'ADD_ITEM_ERROR';
+export const ADD_ITEM_SUCCESS = 'ADD_ITEM_SUCCESS';
+export const REMOVE_ADD_ITEM_ERROR = 'REMOVE_ADD_ITEM_ERROR';
+
+const addItemProcessing = () => {
+    return {
+        type: ADD_ITEM_PROCESSING,
+    }
+}
+
+const addItemSuccess = (item) => {
+    return {
+        type: ADD_ITEM_SUCCESS,
+        item,
+    }
+}
+
+const addItemError = (error) => {
+    return {
+        type: ADD_ITEM_ERROR,
+        error,
+    }
+}
+
+export const removeAddItemError = () => {
+    return {
+        type: REMOVE_ADD_ITEM_ERROR,
+    }
+}
+
+export const handleAddItem = (item) => {
+    return async (dispatch) => {
+        dispatch(addItemProcessing());
+        try {
+            const token = localStorage.getItem('authedUser');
+            const response = await fetch(`http://localhost:8080/items`, {
+                method: 'POST',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                    'Authorization': 'Bearer ' + token,
+                },
+                body: JSON.stringify(item)
+            });
+            if (response.status === 200) {
+                const { data } = await response.json();
+                dispatch(addItemSuccess(data));
+            } else {
+                throw new Error();
+            }
+        } catch {
+            dispatch(addItemError('Error adding item.'))
+        }
+    }
+}
---
+++ src/actions/items/addItem.test.js
@@ -0,0 +1,75 @@
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+
+const middlewares = [thunk];
+const mockStore = configureMockStore(middlewares);
+
+import { 
+    ADD_ITEM_PROCESSING,
+    ADD_ITEM_ERROR,
+    REMOVE_ADD_ITEM_ERROR,
+    ADD_ITEM_SUCCESS,
+    handleAddItem,
+    removeAddItemError,
+} from './addItem';
+
+describe('handleAddItem action creator', () => {
+
+    let store;
+    beforeEach(() => {
+
+        store = mockStore();
+        fetch.resetMocks();
+    });
+
+    it('should dispatch ADD_ITEM_PROCESSING', async () => {
+
+        await store.dispatch(handleAddItem({ name: 'Test Item', type: 'Activity' }));
+        const actions = store.getActions();
+        expect(actions[0]).toEqual({ type: ADD_ITEM_PROCESSING });
+    });
+
+    it('should dispatch ADD_ITEM_SUCCESS when fetch responds with 200 status', async () => {
+
+        fetch.mockResponseOnce(JSON.stringify({ data: { name: 'Test Item' } }), { status: 200 });
+        await store.dispatch(handleAddItem({ name: 'Test Item', type: 'Activity' }));
+        const actions = store.getActions();
+        expect(actions[1]).toEqual({ type: ADD_ITEM_SUCCESS, item: { name: 'Test Item' } });
+    });
+
+    it('should dispatch ADD_ITEM_ERROR when any other status code occurs', async () => {
+
+        fetch.mockResponseOnce(JSON.stringify({}), { status: 400 });
+        await store.dispatch(handleAddItem({ name: 'Test Item', type: 'Activity' }));
+        const actions = store.getActions();
+        expect(actions[1]).toEqual({ 
+            type: ADD_ITEM_ERROR, 
+            error: 'Error adding item.',
+        });
+    });
+
+    it('should dispatch ADD_TAG_ERROR when fetch fails', async () => {
+
+        fetch.mockRejectOnce();
+        await store.dispatch(handleAddItem({ name: 'Test Item', type: 'Activity' }));
+        const actions = store.getActions();
+        expect(actions[1]).toEqual({ 
+            type: ADD_ITEM_ERROR, 
+            error: 'Error adding item.',
+        });
+    });
+
+});
+
+describe('removeAddItemError action creator', () => {
+    
+    it('should dispatch REMOVE_ADD_ITEM_ERROR', () => {
+
+        const store = mockStore();
+        store.dispatch(removeAddItemError());
+        const actions = store.getActions();
+        expect(actions[0]).toEqual({
+            type: REMOVE_ADD_ITEM_ERROR,
+        })
+    })
+});
