75466677f72565671adcb1038fd13e773219f69c
Alex Sligar <info@gravelroad.co>
2018-12-30 19:58:10 -0800

    Display the date variably and use moment for formatting

--- src/components/items/ItemCard.js
+++ src/components/items/ItemCard.js
@@ -1,17 +1,36 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import moment from 'moment';
 import { Card, Icon } from 'semantic-ui-react';
 
+moment.locale('en');
+
 export default function ItemCard (props) {
+    let dateString;
+    if(props.start_date) {
+        dateString = moment(props.start_date).format('lll');
+    }
+    if(props.end_date) {
+        dateString += '-'
+        if(moment(props.start_date).format('l') === moment(props.end_date).format('l')) {
+            dateString += moment(props.end_date).format('h:mm A');
+        } else {
+            dateString += moment(props.end_date).format('lll');
+        }
+    }
+
     return (
         <Card>
             <Card.Content>
                 <Card.Header>{props.name}</Card.Header>
                 <Card.Meta>{props.type}</Card.Meta>
                 <Card.Meta><Icon name='map marker alternate' />{props.location}</Card.Meta>
-                {props.start_date 
-                    ? (<Card.Meta><Icon name='calendar' />{props.start_date} - {props.end_date}</Card.Meta>)
-                    : null
+                {dateString && 
+                    (
+                        <Card.Meta><Icon name='calendar' />
+                            {dateString}
+                        </Card.Meta>
+                    )
                 }
             </Card.Content>
         </Card>
