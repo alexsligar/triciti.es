4d4bcef901100007299f6c7ab56c86cdcbc74be8
Alex Sligar <info@gravelroad.co>
2019-01-01 20:21:38 -0800

    Add ItemDetails stateless functional component and tests

---
+++ src/components/items/ItemDetails.test.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { Card } from 'semantic-ui-react';
+import ItemDetails from './ItemDetails';
+import DateString from './DateString';
+import moment from 'moment';
+
+const setup = (props = {}) => {
+    return shallow(<ItemDetails {...props} />);
+}
+
+describe('render', () => {
+
+    it('should render component without error', () => {
+
+        const wrapper = setup({ item: { type: 'event', location: '123 W. 3rd Ave.', start_date: moment().format() } });
+        const card = wrapper.find(Card);
+        expect(card.length).toBe(1);
+        const dateString = wrapper.find(DateString);
+        expect(dateString.length).toBe(1);
+    });
+
+    it('should not render a DateString if not an event', () => {
+
+        const wrapper = setup({ item: { type: 'activity', location: '123 W. 3rd Ave.' } });
+        const card = wrapper.find(Card);
+        expect(card.length).toBe(1);
+        const dateString = wrapper.find(DateString);
+        expect(dateString.length).toBe(0);
+    });
+});
