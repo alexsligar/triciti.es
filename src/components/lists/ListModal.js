a31d592d27513742fc26a1ae0f790c9cb71b87e4
Alex Sligar <info@gravelroad.co>
2019-01-03 13:56:03 -0800

    Make modal a controlled component that closes when path changes

--- src/components/lists/ListModal.js
+++ src/components/lists/ListModal.js
@@ -1,14 +1,50 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { withRouter } from 'react-router-dom';
 import { Modal, Icon } from 'semantic-ui-react';
 import ListForm from './ListForm';
 
-export default function ListModal() {
-    return (
-        <Modal dimmer='inverted' size='tiny' trigger={<Icon name='list' className='asLink' />}>
-            <Modal.Header>Make a list</Modal.Header>
-            <Modal.Content>
-                <ListForm />
-            </Modal.Content>
-        </Modal>
-    )
+export class ListModal extends Component {
+    state = {
+        open: false,
+    }
+    
+    handleOpen = () => {
+        this.setState({ open: true });
+    }
+
+    handleClose = () => {
+        this.setState({ open: false });
+    }
+
+    componentDidUpdate(prevProps) {
+        if(prevProps.location.pathname !== this.props.location.pathname) {
+            this.handleClose();
+        }
+    }
+
+    render() {
+        return (
+            <Modal 
+                dimmer='inverted' 
+                size='tiny' 
+                trigger={<Icon onClick={this.handleOpen} name='list' className='asLink' />}
+                open={this.state.open}
+                onClose={this.handleClose}    
+            >
+                <Modal.Header>Make a list</Modal.Header>
+                <Modal.Content>
+                    <ListForm />
+                </Modal.Content>
+            </Modal>
+        );
+    }
 }
+
+ListModal.propTypes = {
+    location: PropTypes.shape({
+        pathname: PropTypes.string.isRequired,
+    })
+}
+
+export default withRouter(ListModal)
--- src/components/lists/ListModal.test.js
+++ src/components/lists/ListModal.test.js
@@ -1,17 +1,67 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import { Modal } from 'semantic-ui-react';
-import ListModal from './ListModal';
+import { ListModal } from './ListModal';
 import ListForm from './ListForm';
+
+const defaultProps = {
+    location: {
+        pathname: '/',
+    }
+}
+const setup = (props = {}) => {
+    const propsPassed = { ...defaultProps, ...props };
+    return shallow(<ListModal {...propsPassed} />);
+}
 
 describe('render', () => {
 
     it('should render a Header and ListForm', () => {
 
-        const wrapper = shallow(<ListModal />);
+        const wrapper = setup();
         const header = wrapper.find(Modal.Header);
         expect(header.length).toBe(1);
         const listForm = wrapper.find(ListForm);
         expect(listForm.length).toBe(1);
+    });
+});
+
+describe('handleOpen', () => {
+
+    it('should set state open to true', () => {
+
+        const wrapper = setup();
+        wrapper.instance().handleOpen();
+        expect(wrapper.state().open).toBe(true);
+    });
+});
+
+describe('handleClose', () => {
+
+    it('should set state open to false', () => {
+
+        const wrapper = setup();
+        wrapper.setState({ open: true });
+        wrapper.instance().handleClose();
+        expect(wrapper.state().open).toBe(false);
+    });
+});
+
+describe('componentDidUpdate', () =>{
+
+    it('should set state open to false if path changes', () => {
+        
+        const wrapper = setup();
+        wrapper.setState({ open: true });
+        wrapper.instance().componentDidUpdate({ location: { pathname: '/items/abcd' } });
+        expect(wrapper.state().open).toBe(false);
+    });
+
+    it('should not change state if path doesnt change', () => {
+
+        const wrapper = setup();
+        wrapper.setState({ open: true });
+        wrapper.instance().componentDidUpdate({ location: { pathname: '/' } });
+        expect(wrapper.state().open).toBe(true);
     });
 });
