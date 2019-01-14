68f86595bb2c8528b7f1cd055f5363f9c20f441c
Alex Sligar <info@gravelroad.co>
2019-01-14 11:55:35 -0800

    Prettify files

--- test/testUtils.js
+++ test/testUtils.js
@@ -3,6 +3,6 @@ import { createStore } from 'redux';
 import middleware from '../src/middleware';
 import rootReducer from '../src/reducers';
 
-export const storeFactory  = (initialState) => {
-    return createStore(rootReducer, initialState, middleware);
-}
+export const storeFactory = initialState => {
+  return createStore(rootReducer, initialState, middleware);
+};
