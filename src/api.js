b24c77796a38b19072ed177ffaa5521637e06cd0
Alex Sligar <info@gravelroad.co>
2019-02-26 15:17:05 -0800

    Return await is not needed

--- src/api.js
+++ src/api.js
@@ -1,5 +1,5 @@
 export default async (endpoint, method = 'GET', token = null, body = null) => {
-  return await fetch(`http://localhost:8080/${endpoint}`, {
+  return fetch(`http://localhost:8080/${endpoint}`, {
     method,
     headers: {
       Accept: 'application/json',
